841ccabdf4477498173104046925f20ca197951a
pedrovitorrs <pedro442.rodrigues@gmail.com>
2021-10-13 21:48:41 -0300

    feat: building authentication service

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  constructor(
+    private readonly httpCliente: HttpClient
+    ) { }
+
+  Autheticate(user: string, password: string): Observable<any>
+  {
+    return this.httpCliente.post('http://localhost:3000/user/login',
+    {
+      userName: user,
+      password
+    });
+  }
 }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  // This represents models
+  user = '';
+  password = '';
+
+  constructor(
+    private readonly authService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.authService.Autheticate(this.user, this.password).subscribe(() => {
+      console.log("authenticated successfully!")
+    }, (error) => {
+      alert("User or password is not valid!")
+      console.log(error)
+    }
+    );
+  }
 }
